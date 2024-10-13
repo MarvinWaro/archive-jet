b34a4ccf4b27f020eca8c710e205dd1cf8f22c03
MarvinWaro <mrvnwr.dev@gmail.com>
2024-10-14 00:12:12 +0800

    DONE IN SWALFIRE

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -23,5 +23,13 @@
     <!-- Styles -->
     @livewireStyles
 
+    <!-- SweetAlert CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+
+
+
 </head>
 <body class="font-sans antialiased">
